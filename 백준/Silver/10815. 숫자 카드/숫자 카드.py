c89cc287c6d61c41c11868c8bbf073cd900a86e1
UnbroKen <unbroken2650@gmail.com>
2024-02-28 23:35:17 +0900

    [Silver V] Title: 숫자 카드, Time: 476 ms, Memory: 215488 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+nums = set(map(int, input().rstrip().split(" ")))
+M = int(input().rstrip())
+targets = list(map(int, input().rstrip().split(" ")))
+
+for target in targets:
+    print(1 if target in nums else 0, end=" ")
